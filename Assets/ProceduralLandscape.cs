bf18871edba8f888e9b84b16912985736d870229
jkaloger <jkaloger@users.noreply.github.com>
2017-08-31 18:17:17 +1000

    initialisation of heightmap

--- Assets/ProceduralLandscape.cs
+++ Assets/ProceduralLandscape.cs
@@ -13,24 +13,30 @@ public class ProceduralLandscape : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		this.resolution = Mathf.Pow ((int)scale, 2) + 1;
-		int step = this.resolution / 2;
-
 		DS ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 
+	// diamond square
 	void DS() {
+		int max = this.resolution - 1;
+		// initialise corner values
+		this.heightMap[0,0] = 0f;
+		this.heightMap[0,max] = 0f;
+		this.heightMap[max,0] = 0f;
+		this.heightMap[max,max] = 0f;
 
+		int step = this.resolution / 2;
+
+		// start the diamond step
+		Diamond(0,0,step);
 	}
 
+	// diamond step
 	void Diamond(int x, int y, int step) {
 
 	}
 
+	// square step
 	void Square(int x, int y, int step) {
 
 	}
