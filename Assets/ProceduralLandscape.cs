ab19ffb3411987896582869ef3738f1a3705d861
jkaloger <jkaloger@users.noreply.github.com>
2017-08-31 18:27:27 +1000

    diamond step

--- Assets/ProceduralLandscape.cs
+++ Assets/ProceduralLandscape.cs
@@ -33,11 +33,46 @@ public class ProceduralLandscape : MonoBehaviour {
 
 	// diamond step
 	void Diamond(int x, int y, int step) {
+		int xx, yy;
+		// apply the diamond step
+		xx = Wrap (x + step);
+		yy = Wrap (y + step);
+		heightMap [Wrap (xx), Wrap (yy)] = AvgDiamond (xx, yy);
+		Square (xx, yy, step);
 
+		xx = Wrap (x - step);
+		yy = Wrap (y + step);
+		heightMap [Wrap (xx), Wrap (yy)] = AvgDiamond (xx, yy);
+		Square (xx, yy, step);
+
+		xx = Wrap (x + step);
+		yy = Wrap (y - step);
+		heightMap [Wrap (xx), Wrap (yy)] = AvgDiamond (xx, yy);
+		Square (xx, yy, step);
+
+		xx = Wrap (x - step);
+		yy = Wrap (y - step);
+		heightMap [Wrap (xx), Wrap (yy)] = AvgDiamond (xx, yy);
+		Square (xx, yy, step);
 	}
 
 	// square step
 	void Square(int x, int y, int step) {
 
+	}
+
+	float AvgDiamond(int x, int y) {
+
+
+		return 0;
+	}
+
+	float AvgSquare(int x, int y) {
+
+		return 0;
+	}
+
+	int Wrap(int c) {
+		return (c + this.resolution) % this.resolution;
 	}
 }
