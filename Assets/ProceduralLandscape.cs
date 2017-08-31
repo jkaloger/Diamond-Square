1c8533ec43db5114b8059d2952e0bb734d2452ec
jkaloger <jkaloger@users.noreply.github.com>
2017-08-31 18:15:14 +1000

    diamond_square outline

--- Assets/ProceduralLandscape.cs
+++ Assets/ProceduralLandscape.cs
@@ -4,13 +4,34 @@ using UnityEngine;
 
 public class ProceduralLandscape : MonoBehaviour {
 
+	public int scale = 3;
+	public float maxHeight = 1;
+
+	private int resolution;
+	private float[,] heightMap;
+
 	// Use this for initialization
 	void Start () {
-		
+		this.resolution = Mathf.Pow ((int)scale, 2) + 1;
+		int step = this.resolution / 2;
+
+		DS ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void DS() {
+
+	}
+
+	void Diamond(int x, int y, int step) {
+
+	}
+
+	void Square(int x, int y, int step) {
+
 	}
 }
